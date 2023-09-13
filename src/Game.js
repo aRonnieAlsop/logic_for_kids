eec4805833b96e728fba4336f56724c3eb6ecc77
Arron Alsop <alsop.arron@gmail.com>
2023-09-13 15:41:45 -0700

    'updated'

--- src/Game.js
+++ src/Game.js
@@ -1,28 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Game = () => {
     const [targetNumber, setTargetNumber] = useState(Math.floor(Math.random() * 10) + 1)
     const [selectedNumber, setSelectedNumber] = useState('')
     const [message, setMessage] = useState('')
+    const [isGameOver, setIsGameOver] = useState(false)
 
+    function generateRandomNumber() {
+        return Math.floor(Math.random() * 10) + 1
+    }
+
+    function resetGame() {
+        setTargetNumber(generateRandomNumber()) 
+        setSelectedNumber('')
+        setMessage('')
+        setIsGameOver(false)
+    }
     const handleGuess = () => {
         const guessedNumber = parseInt(selectedNumber)
 
         if (guessedNumber === targetNumber) {
                 setMessage('Congratulations! You guessed the correct number!')
+                setIsGameOver(true)
             } else if (guessedNumber < targetNumber) {
                  setMessage('Try a higher number.')
                 } else {
                     setMessage('Try a lower number.')
                 }
             }
-        
+    useEffect(() => {
+        resetGame()
+    }, [])    
 
         return (
             <div>
-                <h1>Guess the Number Game</h1>
+                <h1>Guess the Number</h1>
                 <p>{message}</p>
-                <label htmFor="numberSelect">Select a number from 1 to 10:</label>
+                <label htmFor="numberSelect">Select a number from 1 to 10:  </label>
                 <select 
                     id="numberSelect"
                     value={selectedNumber}
@@ -35,7 +49,9 @@ const Game = () => {
                         </option>
                     ))}
                     </select>
-                    <button onClick={handleGuess}>Guess</button>
+                    <button onClick={handleGuess} disabled={isGameOver}>Guess</button><br />
+                    <br />
+                    <button onClick={resetGame}>Play Again</button>
             </div>
         )
         }
